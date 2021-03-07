67ac04f4082e85b938541d90fe7656fa54c46f1a
Daniel Vaini <dnlvaini@gmail.com>
2021-03-07 16:35:03 -0300

    feat: Add every modules exported

--- Test/calc.js
+++ Test/calc.js
@@ -1,4 +1,9 @@
-const somaFunc = require('./somar');
+const SomaFunc = require('./somar');
+const SubFunc = require('./subtrair')
+const MultFunc = require('./multiplicar')
+const DivFunc = require('./div')
 
-
-console.log(somaFunc(1, 2))
+console.log(SomaFunc(1, 2))
+console.log(SubFunc(2, 1))
+console.log(MultFunc(2, 1))
+console.log(DivFunc(2, 1))
