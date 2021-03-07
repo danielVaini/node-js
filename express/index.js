d8ea260ae461b5fdf5fa59671495c52cb6b02a03
Daniel Vaini <dnlvaini@gmail.com>
2021-03-07 17:47:37 -0300

    feat: Creating new routes with get

--- express/index.js
+++ express/index.js
@@ -5,6 +5,13 @@ app.get('/', (req, res) => {
   res.send('Hello worlld')
 })
 
+app.get('/sobre', (req, res) => {
+  res.send('Minha pagina sobre')
+})
+
+app.get('/blog', (req, res) => {
+  res.send('Bem vindo ao meu blog')
+})
 
 app.listen(3333,() => {
   console.log('Conected')
