7fa546e4983305c34a94d76193da470efa833477
Daniel Vaini <dnlvaini@gmail.com>
2021-03-09 16:20:04 -0300

    feat: Setting handlebars

--- express-nodemon/index.js
+++ express-nodemon/index.js
@@ -1,25 +1,11 @@
 const express = require('express');
 const app = express();
+const handlebars = require('express-handlebars');
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + "/html/index.html")
-})
-
-
-app.get('/sobre', (req, res) => {
-  res.sendFile(__dirname + '/html/sobre.html')
-})
-
-app.get('/blog', (req, res) => {
-  res.send('Bem vindo ao meu blog')
-})
-
-app.get('/ola/:nome/:cargo/:cor', (req, res) => {
-  res.send('<h1> Ola ' + req.params.nome + '</h1>' +
-  '<h2> Seu cargo e: '+ req.params.cargo + '</h2>' +
-  '<h3> Sua cor favorita e: '+ req.params.cor + '</h3>'); // Send só envia uma vez por rota
-
-})
+// Config
+  // Template Engine
+    app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+    app.set('view engine', 'handlebars')
 
 app.listen(3333,() => {
   console.log('Conected')
