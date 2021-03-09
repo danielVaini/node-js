ca12b93b756184b123c92b2c3eb3221d7b888009
Daniel Vaini <dnlvaini@gmail.com>
2021-03-09 16:27:21 -0300

    feat: Adding 'sequelize' connection with database

--- express-nodemon/index.js
+++ express-nodemon/index.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const app = express();
 const handlebars = require('express-handlebars');
+const Sequelize = require('sequelize').Sequelize;
+
+
+
 
 // Config
   // Template Engine
     app.engine('handlebars', handlebars({defaultLayout: 'main'}))
     app.set('view engine', 'handlebars')
+
+  // Connecting with database
+  const sequelize = new Sequelize('test', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql'
+  });
 
 app.listen(3333,() => {
   console.log('Conected')
