06478f183b0564e982b5243753bf89a2c9dd115c
Daniel Vaini <dnlvaini@gmail.com>
2021-03-09 16:14:46 -0300

    feat: Creating table 'Usuarios' and inserting data

--- express-nodemon/test.js
+++ express-nodemon/test.js
@@ -21,8 +21,33 @@ const Postagem = sequelize.define('postagens', {
 
 // Inserir dados
 
-Postagem.create({
-  titulo: 'Titulo qualquer',
-  conteudo: 'asdasdasdasdsa'
+// Postagem.create({
+//   titulo: 'Titulo qualquer',
+//   conteudo: 'asdasdasdasdsa'
+// })
+
+const Usuario = sequelize.define('usuarios', {
+  nome: {
+    type: Sequelize.STRING
+  },
+  sobrenome: {
+    type: Sequelize.STRING,
+  },
+  idade: {
+    type: Sequelize.INTEGER,
+  },
+  email: {
+    type: Sequelize.STRING,
+  }
 })
 
+// Usuario.sync({force: true})
+
+// Inserir dados na tabela Usuarios
+
+Usuario.create({
+  nome: 'Daniel',
+  sobrenome: 'Santos',
+  idade: 20,
+  email: 'dnl@santos.com'
+})
