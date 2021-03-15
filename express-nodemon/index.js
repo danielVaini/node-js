c28e99df2ee29e0d6dff699fbbad4e09229eedee
Daniel Vaini <dnlvaini@gmail.com>
2021-03-14 23:33:21 -0300

    feat: Creating a new route for home page

--- express-nodemon/index.js
+++ express-nodemon/index.js
@@ -2,32 +2,47 @@ const express = require('express');
 const app = express();
 const handlebars = require('express-handlebars');
 
-
-
-
-
+const Post = require('./models/Post');
 
 // Config
-// Template Engine
-app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
-app.set('view engine', 'handlebars')
-// Body parser
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
+  // Template Engine
+  app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
+  app.set('view engine', 'handlebars')
+  // Body parser
+  app.use(express.urlencoded({ extended: false }))
+  app.use(express.json())
 
 
 
 // Routes
 
-app.get('/cad', (req, res) => {
-  res.render('formulario')
-})
+  app.get('/',(req, res) => {
+    Post.findAll().then((posts) => {
+      res.render('home', {
+        post: posts
+      });
 
-app.post('/add', (req, res) => {
-  req.body.conteudo
-  res.send(`Texto: ${req.body.titulo} Conteudo: ${req.body.conteudo}`)
-})
+    })
+  })
 
-app.listen(3333, () => {
-  console.log('Conected')
-})
+  app.get('/cad', (req, res) => {
+    res.render('formulario')
+  })
+
+  app.post('/add', (req, res) => {
+
+    Post.create({
+      titulo: req.body.titulo,
+      conteudo: req.body.conteudo
+    }).then(()=> {
+      res.redirect('/')
+    }).catch((err) => {
+      res.send('Error: '+err)
+    })
+   
+    // res.send(`Texto: ${req.body.titulo} Conteudo: ${req.body.conteudo}`)
+  })
+
+  app.listen(3333, () => {
+    console.log('Conected')
+  })
